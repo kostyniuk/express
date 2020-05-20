4add15eefeff282eaecbfed4f1e68a4e506011ec
kostyniuk <kostyniuk7@gmail.com>
2020-05-20 17:21:00 +0300

    signup is done

--- social-network/server/routes/signup.js
+++ social-network/server/routes/signup.js
@@ -2,9 +2,13 @@
 'use strict';
 
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const db = require('../db/db');
 
 const router = express.Router();
+
+const salt = bcrypt.genSaltSync(10);
+
 
 router.get('/', (req, res, next) => {
   console.log({ req });
@@ -14,12 +18,20 @@ router.get('/', (req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     const {email, fullName, age, username, password} = req.body;
-    console.log(2)
-    console.log({email, fullName, age, username, password})
-    const text = `INSERT INTO person (age, fullName, email) VALUES ($1, $2, $3);`
-    const values = [age, fullName, email]
-    const result = await db.query(text, values);
-    console.log({result});
+    console.log({email, fullName, age, username, password});
+
+    const hash = bcrypt.hashSync(password, salt);
+
+    const queryUser = `INSERT INTO user_info (username, password) VALUES ($1, $2) RETURNING user_id;`
+    const valuesUser = [username, hash]
+    let {rows} = await db.query(queryUser, valuesUser);
+    const id = rows[0].user_id
+    const queryPerson = `INSERT INTO person (person_id, age, fullName, email) VALUES ($1, $2, $3, $4);`
+    const valuesPerson = [id, age, fullName, email]
+    const result = await db.query(queryPerson, valuesPerson);
+
+    console.log({rows});
+    console.log({id, person: result.rows[0]});
     res.json('Data obtained');
   } catch(e) {
     console.error({ e })
