9314ca957c104f58bbf8340169cabd92ca9fb65d
kostyniuk <kostyniuk7@gmail.com>
2020-05-20 19:23:53 +0300

    nav connected

--- social-network/client/src/App.js
+++ social-network/client/src/App.js
@@ -1,11 +1,13 @@
 import React, {Fragment} from 'react';
 import './App.css';
 
-import InputSignUp from './components/InputSignUp'
+import InputSignUp from './components/InputSignUp';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Fragment> 
+        <NavBar />
       <div className='container'>
         <InputSignUp /> 
       </div>
