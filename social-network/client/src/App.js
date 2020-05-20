2c444ebd3d1879c7bc3e3468ee92d103ce6c687f
kostyniuk <kostyniuk7@gmail.com>
2020-05-20 19:34:54 +0300

    route to signup is implemented

--- social-network/client/src/App.js
+++ social-network/client/src/App.js
@@ -1,17 +1,21 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import InputSignUp from './components/InputSignUp';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <Fragment> 
-        <NavBar />
-      <div className='container'>
-        <InputSignUp /> 
-      </div>
+    <Router>
+      <Fragment>
+        <div className='container'>
+          <Switch>
+          <Route path='/signup' exact component={InputSignUp} />
+          </Switch>
+        </div>
       </Fragment>
+    </Router>
   );
 }
 
--- social-network/server/routes/signup.js
+++ social-network/server/routes/signup.js
@@ -11,25 +11,24 @@ const salt = bcrypt.genSaltSync(10);
 
 router.post('/', async (req, res, next) => {
   try {
-
-    const {email, fullName, age, username, password} = req.body;
+    const { email, fullName, age, username, password } = req.body;
 
     const hash = bcrypt.hashSync(password, salt);
 
-    const queryUser = `INSERT INTO user_info (username, password) VALUES ($1, $2) RETURNING user_id;`
-    const valuesUser = [username, hash]
-    let {rows} = await db.query(queryUser, valuesUser);
+    const queryUser = `INSERT INTO user_info (username, password) VALUES ($1, $2) RETURNING user_id;`;
+    const valuesUser = [username, hash];
+    let { rows } = await db.query(queryUser, valuesUser);
 
-    const id = rows[0].user_id
+    const id = rows[0].user_id;
 
-    const queryPerson = `INSERT INTO person (person_id, age, fullName, email) VALUES ($1, $2, $3, $4);`
-    const valuesPerson = [id, age, fullName, email]
+    const queryPerson = `INSERT INTO person (person_id, age, fullName, email) VALUES ($1, $2, $3, $4);`;
+    const valuesPerson = [id, age, fullName, email];
     const result = await db.query(queryPerson, valuesPerson);
 
     res.json('Data obtained');
-  } catch(e) {
-    console.error({ e })
+  } catch (e) {
+    console.error({ e });
   }
-})
+});
 
 module.exports = router;
