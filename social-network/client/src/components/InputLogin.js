0930a342216f429e9824b23e8ea64a2ab96c9c1d
kostyniuk <kostyniuk7@gmail.com>
2020-05-21 07:35:05 +0300

    add request

--- social-network/client/src/components/InputLogin.js
+++ social-network/client/src/components/InputLogin.js
@@ -4,11 +4,27 @@ const InputLogIn = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { username, password };
+      const response = await fetch('http://localhost:4000/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      });
+
+      window.location = '/';
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
     <Fragment>
       <h1 className='text-center mt-5 text-white'>Log In</h1>
       <div className='col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4'>
-        <form className='m-5'>
+        <form className='m-5' onSubmit={onSubmitForm}>
           <input
             type='text'
             className='form-control mb-3'
