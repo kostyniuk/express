ab1a93b9cc3fab72489808e5f49b0daab940efda
kostyniuk <kostyniuk7@gmail.com>
2020-05-25 16:41:26 +0300

    redirection if error added

--- social-network/server/routes/login.js
+++ social-network/server/routes/login.js
@@ -6,7 +6,7 @@ const passport = require('passport');
 const router = express.Router();
 
 // eslint-disable-next-line max-len
-router.post('/', passport.authenticate('local', { failureRedirect: '/api/user/dloading', successRedirect: '/api/user/jimmybutler' }));
+router.post('/', passport.authenticate('local', { failureRedirect: '/api/login/failure', successRedirect: '/api/login/success' }));
 //try {
 // const { username, password } = req.body;
 // console.log({username, password})
@@ -35,5 +35,14 @@ router.post('/', passport.authenticate('local', { failureRedirect: '/api/user/dl
 //   console.error({ e });
 // }
 //});
+
+router.get('/failure', (req, res, next) => {
+  res.json({ e: 'Authorization failed' });
+});
+
+router.get('/success', (req, res, next) => {
+  console.log(req.session);
+  res.json({ e: 'Authorization successfull' });
+});
 
 module.exports = router;
