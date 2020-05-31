8fff9ceaea548640da05d7eb9562ab938f35a8d1
kostyniuk <kostyniuk7@gmail.com>
2020-05-31 14:18:37 +0300

    get endpoint for post

--- social-network/server/routes/post.js
+++ social-network/server/routes/post.js
@@ -6,27 +6,45 @@ const db = require('../config/db');
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json([{caption: 'dasdas', date: 'dadas', likes: 3}, {caption: 'Hi', date: '12.03.21', likes: 13}])
-})
-
-router.post('/', async (req, res, next) => {
+router.get('/:username', async (req, res, next) => {
   try {
-    const { caption } = req.body;
-    
-    const { user_id } = req.user;
+    const { username } = req.params;
+    console.log({ username });
 
-    const query = `INSERT INTO post (creator_id, caption) VALUES ($1, $2);`
-    const params = [user_id, caption];
+    const query = `SELECT post.caption, post.created_at, post.number_of_likes 
+    FROM Post
+    JOIN user_info ON user_info.user_id = post.creator_id
+    WHERE user_info.username = $1
+    ORDER BY post.post_id DESC;`;
+    const parametrs = [username];
 
-    const {rows} = await db.query(query, params);
-    console.log({rows})
-
-    res.json({ message: 'The post was successfully created', username: req.user.username });
+    const { rows } = await db.query(query, parametrs);
+    console.log({ posts: rows });
+    res.json(rows);
   } catch (e) {
     console.error(e);
   }
 });
 
+router.post('/', async (req, res, next) => {
+  try {
+    const { caption } = req.body;
+
+    const { user_id } = req.user;
+
+    const query = `INSERT INTO post (creator_id, caption) VALUES ($1, $2);`;
+    const params = [user_id, caption];
+
+    const { rows } = await db.query(query, params);
+    console.log({ rows });
+
+    res.json({
+      message: 'The post was successfully created',
+      username: req.user.username,
+    });
+  } catch (e) {
+    console.error(e);
+  }
+});
 
 module.exports = router;
