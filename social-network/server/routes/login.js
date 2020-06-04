f6be3c4161062b063983a6b2f80033f3f778b4f8
kostyniuk <kostyniuk7@gmail.com>
2020-06-04 11:22:27 +0300

    dress it up a little bit

--- social-network/server/routes/login.js
+++ social-network/server/routes/login.js
@@ -7,34 +7,6 @@ const router = express.Router();
 
 // eslint-disable-next-line max-len
 router.post('/', passport.authenticate('local', { failureRedirect: '/api/login/failure', successRedirect: '/api/login/success' }));
-//try {
-// const { username, password } = req.body;
-// console.log({username, password})
-// const queryUser = `SELECT password from user_info WHERE username = $1;`;
-// const valuesUser = [username];
-// let { rows } = await db.query(queryUser, valuesUser);
-// console.log({ rows });
-
-
-// if (rows.length === 0) {
-//   res.status(404).json('Not Authenthificated');
-// }
-
-// const hashedDB = rows[0].password;
-
-// const checkPassword = await bcrypt.compare(password, hashedDB);
-
-// if (checkPassword) {
-//   req.session.username = username;
-//   res.json('Authenthificated');
-// } else {
-//   res.json('Not Authenthificated');
-
-//}
-// } catch (e) {
-//   console.error({ e });
-// }
-//});
 
 router.get('/failure', (req, res, next) => {
   res.json({ e: 'Authorization failed' });
