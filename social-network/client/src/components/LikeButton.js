3ba8929b89169fa842b12eabc7662cec61d342a0
kostyniuk <kostyniuk7@gmail.com>
2020-06-07 15:10:47 +0300

    dress it up

--- social-network/client/src/components/LikeButton.js
+++ social-network/client/src/components/LikeButton.js
@@ -1,33 +1,12 @@
-import React, { Fragment, useState } from 'react';
+import React from 'react';
 
-const LikeButton = () => {
-  const [likes, setLikes] = useState(0);
-
-  const addLike = () => {
-    if (likes) {
-      setLikes(0);
-    } else {
-      setLikes(likes + 1);
-    }
-  };
-
-  if (likes === 0) {
-    return (
-      <div>
-        <button className='button' onClick={addLike}>
-          <i className='far fa-heart fa-lg' style={{ color: 'white' }}></i>
-        </button>
-      </div>
-    );
-  }
-  if (likes === 1) {
-    return (
-      <div>
-        <button className='button' onClick={addLike}>
-          <i className='fas fa-heart fa-lg' style={{ color: 'red' }}></i>
-        </button>
-      </div>
-    );
-  }
-}
+const LikeButton = ({ handler, buttonColor }) => {
+  return (
+    <div>
+      <button className='button' onClick={handler}>
+        <i className='far fa-heart fa-lg' style={{ color: buttonColor }}></i>
+      </button>
+    </div>
+  );
+};
 export default LikeButton;
