bf3b9f73b2415140dfb9219a36734653c082cfc1
kostyniuk <kostyniuk7@gmail.com>
2020-06-08 17:58:38 +0300

    change profile picture style

--- social-network/client/src/components/BioComponent.js
+++ social-network/client/src/components/BioComponent.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 const Bio = ({ image, name, email, age, numberOfPosts, bio }) => {
   return (
-    <div className='col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto form p-4col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto form p-4 text-wrap text-white'>
+    <div className='col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto form p-4 col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto form p-4 text-wrap text-white'>
       <form className='m-2 bg-dark'>
-        <img
-          src={image}
-          width='200'
-          height='200'
-          class='rounded-circle z-depth-2 mx-auto d-block '
-        />
+        &nbsp;
+        <div className='containerP'>
+          <img
+            src={image}
+            title='Change profile picture'
+            width='200'
+            height='200'
+            alt='sfd'
+            style={{ cursor: 'pointer' }}
+            class='profile-picture rounded-circle z-depth-2 mx-auto d-block '
+            onClick={() => {
+              console.log('clicked');
+            }}
+          />
+        </div>
         <ul className=''>
           <li className='mt-3'>Name: {name}</li>
           <li className='mt-3'>Email: {email}</li>
@@ -17,6 +26,7 @@ const Bio = ({ image, name, email, age, numberOfPosts, bio }) => {
           <li className='mt-3'>Posts: {numberOfPosts}</li>
           <li className='mt-3'>Bio: {bio}</li>
         </ul>
+        &nbsp;
       </form>
     </div>
   );
