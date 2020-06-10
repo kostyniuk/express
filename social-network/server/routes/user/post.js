d5aa2afd2cd0b997bb777af74ef8adbb59b8f727
kostyniuk <kostyniuk7@gmail.com>
2020-06-10 20:32:59 +0300

    dress it up a little bit

--- social-network/server/routes/user/post.js
+++ social-network/server/routes/user/post.js
@@ -1,16 +1,14 @@
 'use strict';
 
 const express = require('express');
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 const isAvailable = require('../../lib/isOwnPage');
 const db = require('../../config/db');
-
 
 // posts handling
 router.get('/', async (req, res, next) => {
   try {
     const { nickname } = req.params;
-    console.log({par: req.params})
 
     const query = `SELECT post.post_id, post.caption, post.created_at, post.number_of_likes 
     FROM Post
@@ -25,6 +23,7 @@ router.get('/', async (req, res, next) => {
     console.error(e);
   }
 });
+
 
 router.post('/', isAvailable, async (req, res, next) => {
   try {
