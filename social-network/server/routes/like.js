795daebb2e6a3eb8cdac9a24e118957d860f2c01
kostyniuk <kostyniuk7@gmail.com>
2020-06-11 14:35:47 +0300

    add handling errors

--- social-network/server/routes/like.js
+++ social-network/server/routes/like.js
@@ -25,28 +25,31 @@ router.get('/:postId', async (req, res, next) => {
       WHERE p.person_id in (${addParameters})`;
       const params2 = userIds;
       const info = await db.query(query2, params2);
-      const data = info.rows
+      const data = info.rows;
       res.status(200).json(data);
+    } else {
+      res.status(200).json(rows);
     }
-    res.status(200).json(rows);
   } catch (e) {
     console.error(e);
   }
 });
 
-// router.post('/:postId', (req, res, next) => {
-//   try {
-//     const { postId } = req.params;
-//     console.log({ postId });
-//     const query = `SELECT * FROM Likes WHERE post_id = $1`;
-//     const params = [postId];
+router.post('/:postId', async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+    const { user_id } = req.user;
 
-//     const { rows } = await db.query(query, params);
-//     console.log({ rows });
-//     res.status(200).json(rows);
-//   } catch (e) {
-//     console.error(e);
-//   }
-// });
+    console.log({ postId, user_id });
+    const query = `INSERT INTO Likes (post_id, from_id) VALUES ($1, $2)`;
+    const params = [postId, user_id];
+    const {rows} = await db.query(query, params)
+    console.log({rows})
+    res.status(200).json({user_id, postId, rows});
+  } catch (e) {
+    console.error(e);
+    res.status(409).json(e)
+  }
+});
 
 module.exports = router;
