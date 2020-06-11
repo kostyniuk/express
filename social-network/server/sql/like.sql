f59872f89d5d5260a53ff35d30f9b040d001aa36
kostyniuk <kostyniuk7@gmail.com>
2020-06-11 14:33:06 +0300

    add unique constraint

--- social-network/server/sql/like.sql
+++ social-network/server/sql/like.sql
@@ -15,3 +15,5 @@ ALTER TABLE Likes ADD CONSTRAINT fk_Like_Post_Id
 ALTER TABLE Likes ADD CONSTRAINT fk_Like_User_Id
   FOREIGN KEY (from_id) REFERENCES user_info (user_id);
   
+ALTER TABLE Likes ADD CONSTRAINT one_like_for_one_post_from_one_user
+ UNIQUE(post_id, from_id);
