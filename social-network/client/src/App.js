78c84306db3233b9aef0629d1933a6f34b868a0b
kostyniuk <kostyniuk7@gmail.com>
2020-06-12 15:36:15 +0300

    add context hook to transfer logged in username

--- social-network/client/src/App.js
+++ social-network/client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect, createContext } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -13,8 +13,11 @@ import InputLogIn from './components/InputLogin';
 import InputPost from './components/inputPost';
 import User from './components/User';
 
+export const LoggedInUserContext = createContext('');
+
 function App() {
   const [user, setUser] = useState('');
+  const [path, setPath] = useState('');
 
   useEffect(() => {
     fetchUser();
@@ -23,23 +26,28 @@ function App() {
   const fetchUser = async () => {
     const responce = await fetch('http://localhost:3000/api/whoami');
     const data = await responce.json();
+    setUser(data.user);
     if (data.authentificated) {
-      setUser('user/' + data.user);
+      setPath('user/' + data.user);
     } else {
-      setUser('login');
+      setPath('login');
     }
   };
+
+  console.log({ path, user });
 
   return (
     <Router>
       <Fragment>
         <div className='container'>
           <Switch>
-            <Redirect path='/' exact to={`${user}`} />
+            <Redirect path='/' exact to={`${path}`} />
             <Route path='/signup' exact component={InputSignUp} />
             <Route path='/login' exact component={InputLogIn} />
             <Route path='/user' exact component={NavBar} />
-            <Route path='/user/:username' exact component={User} />
+            <LoggedInUserContext.Provider value={user}>
+              <Route path='/user/:username' exact component={User} />
+            </LoggedInUserContext.Provider>
             <Route
               path='/user/:username/createPost'
               exact
