87ed796665597fde48ac454d648793cedc24b2f6
kostyniuk <kostyniuk7@gmail.com>
2020-06-12 18:58:54 +0300

    add loading component

---
+++ social-network/client/src/components/Loading.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className='lds-spinner d-flex justify-content-center align-items-center mt-5 h-100'>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loading;
