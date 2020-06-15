a9400b52c4f506f20119bf5f9403df90420d1b69
kostyniuk <kostyniuk7@gmail.com>
2020-06-15 11:53:14 +0300

    implement redirect to another profile

--- social-network/client/src/components/LikedUser.js
+++ social-network/client/src/components/LikedUser.js
@@ -1,31 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
 
 const LikedUser = ({ user }) => {
+
+  const [redirect, setRedirect] = useState(false)
+
+  if (redirect) window.location.assign(`${user.username}`);
+    
   return (
     <div>
       <div className='button-wrapper d-flex justify-content-start'>
-        <img
-          src={'http://localhost:3000/api' + user.picture.substring(1)}
-          title={user.username} // TODO
-          width='50'
-          height='50'
-          alt='sfd'
-          style={{ cursor: 'pointer' }}
-          className='profile-picture visible rounded-circle z-depth-2 mx-auto d-block '
-        />
-        <span
-          className='label username-likes'
-          style={{
-            color: 'white',
-            textTransform: 'none',
-            padding: '15px 0px',
-            textAlign: 'left',
-            marginLeft: '10px',
-            fontSize: '15px',
-          }}
-        >
-          {user.username}
-        </span>
+        <button className='invisible' onClick={() => setRedirect(true)}>
+          <div className='visible d-flex justify-content-start'>
+            <img
+              src={'http://localhost:3000/api' + user.picture.substring(1)}
+              title={user.username} // TODO
+              width='50'
+              height='50'
+              alt='sfd'
+              style={{ cursor: 'pointer' }}
+              className='profile-picture visible rounded-circle z-depth-2 mx-auto d-block '
+            />
+            <span
+              className='label username-likes'
+              style={{
+                color: 'white',
+                textTransform: 'none',
+                padding: '15px 0px',
+                textAlign: 'left',
+                marginLeft: '10px',
+                fontSize: '15px',
+              }}
+            >
+              {user.username}
+            </span>
+          </div>
+        </button>
       </div>
       <hr style={{ background: 'white' }}></hr>
     </div>
