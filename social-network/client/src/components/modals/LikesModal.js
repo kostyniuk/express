b0a0272033f80a951ae4165abec4b94e0a1e818f
kostyniuk <kostyniuk7@gmail.com>
2020-06-15 11:34:26 +0300

    add LikedUser component

--- social-network/client/src/components/modals/LikesModal.js
+++ social-network/client/src/components/modals/LikesModal.js
@@ -1,65 +1,63 @@
 import React, { useState, Fragment } from 'react';
 
-const LikesClickModal = ({ number_of_likes, postId, info }) => {
+import LikedUser from '../LikedUser';
+
+const LikesClickModal = ({ number_of_likes, postId, info, show }) => {
   console.log({ postId, info });
 
   const [post, setPost] = useState(postId);
+  const [usersInfo, setUserInfo] = useState(info);
 
-  // console.log({id: info.data[0].person_id})
+  console.log({ usersInfo, info });
 
-  return (
-    <Fragment>
-      <button
-        type='button'
-        className='invisible'
-        data-toggle='modal'
-        data-target={`#id${postId}`}
-      >
-        <p className='visible text-white'>{number_of_likes} likes</p>
-      </button>
-      <div
-        className='modal fade'
-        id={`id${postId}`}
-        tabIndex='-1'
-        role='dialog'
-        aria-labelledby='a'
-        aria-hidden='true'
-      >
-        <div className='modal-dialog modal-dialog-centered' role='document'>
-          <div className='modal-content' style={{ background: '#282c34' }}>
-            <div className='modal-header'>
-              <h5 className='modal-title' id='exampleModalLongTitle'>
-                Likes: {post}
-              </h5>
-              <button
-                type='button'
-                className='close text-white'
-                data-dismiss='modal'
-                aria-label='Close'
-              >
-                <span aria-hidden='true'>&times;</span>
-              </button>
-            </div>
-            <div className='modal-body'>
-              <div className='button-wrapper'>
-                <span className='label'>sad: {postId}</span>
-
-                <input
-                  title=''
-                  type='file'
-                  name='profilePhoto'
-                  id='upload'
-                  className='upload-box'
-                  placeholder='Upload File'
-                />
+  if (show) {
+    return (
+      <Fragment>
+        <button
+          type='button'
+          className='invisible'
+          data-toggle='modal'
+          data-target={`#id${postId}`}
+        >
+          <p className='visible text-white'>{number_of_likes} likes</p>
+        </button>
+        <div
+          className='modal fade'
+          id={`id${postId}`}
+          tabIndex='-1'
+          role='dialog'
+          aria-labelledby='a'
+          aria-hidden='true'
+        >
+          <div className='modal-dialog modal-dialog-centered' role='document'>
+            <div className='modal-content' style={{ background: '#282c34' }}>
+              <div className='modal-header'>
+                <h5 className='modal-title' id='exampleModalLongTitle'>
+                  Likes
+                </h5>
+                <button
+                  type='button'
+                  className='close text-white'
+                  data-dismiss='modal'
+                  aria-label='Close'
+                >
+                  <span aria-hidden='true'>&times;</span>
+                </button>
               </div>
-              <hr style={{ background: 'white' }}></hr>
+
+              <div className='modal-body'>
+                {info.map((user) => (
+                  <LikedUser user={user} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Fragment>
-  );
+      </Fragment>
+    );
+  } else {
+    return '';
+  }
 };
 
 export default LikesClickModal;
