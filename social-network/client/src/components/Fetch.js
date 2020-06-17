1397c056b3eb4a1ed5ff342cfadb85434fd8d1e5
kostyniuk <kostyniuk7@gmail.com>
2020-06-17 16:41:44 +0300

    fetching users component

---
+++ social-network/client/src/components/Fetch.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import SelectSearch from 'react-select-search';
+
+const Fetch = () => (
+  <SelectSearch
+    options={[]}
+    getOptions={(query) => {
+      return new Promise((resolve, reject) => {
+        fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`
+        )
+          .then((response) => response.json())
+          .then(({ drinks }) => {
+            resolve(
+              drinks.map(({ idDrink, strDrink }) => ({
+                value: idDrink,
+                name: strDrink,
+              }))
+            );
+          })
+          .catch(reject);
+      });
+    }}
+    search
+    placeholder='Your favorite drink'
+  />
+);
+
+export default Fetch;
