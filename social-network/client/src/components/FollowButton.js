b6ebb1fb4efcba758b31d48f48e6038f569d589c
kostyniuk <kostyniuk7@gmail.com>
2020-06-17 17:02:13 +0300

    add a toggle

--- social-network/client/src/components/FollowButton.js
+++ social-network/client/src/components/FollowButton.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const FollowButton = () => {
+const FollowButton = ({ followed }) => {
+
+  const [follow, setFollow] = useState(followed || false)
+
+  const toggleColor = () => {
+    setFollow(prev => !prev)
+  }
+
   return (
     <div className='ml-auto p-2 d-flex'>
-      <button className='btn btn-primary'> Follow </button>
+      <button className='btn btn-primary' className={follow ? 'btn-secondary' : 'btn-primary'}  onClick={() => toggleColor()}> {follow ? 'following' : 'follow'} </button>
     </div>
   );
 };
