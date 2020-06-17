bcac0d93f96945eb1f3ff03fbb91886a700f029d
kostyniuk <kostyniuk7@gmail.com>
2020-06-17 16:41:32 +0300

    add follow button

---
+++ social-network/client/src/components/FollowButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FollowButton = () => {
+  return (
+    <div className='ml-auto p-2 d-flex'>
+      <button className='btn btn-primary'> Follow </button>
+    </div>
+  );
+};
+
+export default FollowButton;
--- social-network/client/src/components/LikedUser.js
+++ social-network/client/src/components/LikedUser.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
+import FollowButton from './FollowButton';
 
 const LikedUser = ({ user }) => {
-
-  const [redirect, setRedirect] = useState(false)
+  const [redirect, setRedirect] = useState(false);
 
   if (redirect) window.location.assign(`${user.username}`);
-    
+
   return (
     <div>
-      <div className='button-wrapper d-flex justify-content-start'>
-        <button className='invisible' onClick={() => setRedirect(true)}>
+      <div className='button-wrapper d-flex'>
+        <button className='invisible' style={{width: '100%'}}  onClick={() => setRedirect(true)}>
           <div className='visible d-flex justify-content-start'>
             <img
               src={'http://localhost:3000/api' + user.picture.substring(1)}
@@ -33,6 +33,7 @@ const LikedUser = ({ user }) => {
             >
               {user.username}
             </span>
+            <FollowButton />
           </div>
         </button>
       </div>
