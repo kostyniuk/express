806c07e09b548929214f994fb0a24b021060eaeb
kostyniuk <kostyniuk7@gmail.com>
2020-06-19 10:10:24 +0300

    add Follow Button component

--- social-network/client/src/components/FollowButton.js
+++ social-network/client/src/components/FollowButton.js
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { LoggedInUserContext } from '../App';
 
-const FollowButton = ({ followed }) => {
+const FollowButton = ({ followed, followWho }) => {
+  const user = useContext(LoggedInUserContext);
+  const [follow, setFollow] = useState(followed || false);
 
-  const [follow, setFollow] = useState(followed || false)
+  const toggleColor = async () => {
+    setFollow((prev) => !prev);
 
-  const toggleColor = () => {
-    setFollow(prev => !prev)
-  }
+    const url = `http://localhost:3000/api/follow/${followWho}`;
+    const method = follow ? 'DELETE' : 'POST';
+
+    const data = await fetch(url, {
+      method,
+    });
+
+    const json = await data.json();
+    console.log({ json });
+  };
 
   return (
     <div className='ml-auto p-2 d-flex'>
-      <button className='btn btn-primary' className={follow ? 'btn-secondary' : 'btn-primary'}  onClick={() => toggleColor()}> {follow ? 'following' : 'follow'} </button>
+      <button
+        className='btn btn-primary'
+        className={follow ? 'btn-secondary' : 'btn-primary'}
+        onClick={() => toggleColor()}
+      >
+        {' '}
+        {follow ? 'following' : 'follow'}{' '}
+      </button>
     </div>
   );
 };
