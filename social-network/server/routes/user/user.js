2f25a6a7ecdb5c080349ce8313d3df8275462ce8
kostyniuk <kostyniuk7@gmail.com>
2020-06-19 10:09:39 +0300

    add possibility to change a bio

--- social-network/server/routes/user/user.js
+++ social-network/server/routes/user/user.js
@@ -3,14 +3,14 @@
 const express = require('express');
 
 const { findIdByUserName, personInfoById } = require('../../lib/sqlUtils');
+const db = require('../../config/db');
 
 const postRoute = require('./post');
 const profilePictureRoute = require('./profilePicture');
-
+const isLoggedIn = require('../../lib/isLoggedIn');
 const router = express.Router();
 
 // multer config
-
 
 //users handling
 router.get('/:nickname', async (req, res, next) => {
@@ -24,6 +24,23 @@ router.get('/:nickname', async (req, res, next) => {
     res.json({ nickname, info });
   } catch (e) {
     console.error(e);
+  }
+});
+
+router.post('/bio', isLoggedIn, async (req, res, next) => {
+  try {
+    const { bio } = req.body;
+    const { user_id } = req.user;
+
+    const query = `UPDATE Person SET bio = $1 WHERE person_id = $2;`;
+    const params = [bio, user_id];
+
+    const result = await db.query(query, params);
+    console.log({ result });
+
+    res.status(200).json({ message: `Bio updated: '${bio}'` });
+  } catch (e) {
+    console.error({ e });
   }
 });
 
