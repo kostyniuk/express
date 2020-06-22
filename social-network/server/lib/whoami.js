2366c883c4de52722ede5ddeb957eb62d5f242f9
kostyniuk <kostyniuk7@gmail.com>
2020-06-22 14:48:16 +0300

    change a little bit

--- social-network/server/lib/whoami.js
+++ social-network/server/lib/whoami.js
@@ -3,7 +3,7 @@
 module.exports = (req, res, next) => {
 
   if (req.user) {
-    return res.json({ authentificated: true, user: req.user.username });
+    return res.json({ authentificated: true, user: req.user.username, id: req.user.user_id });
   }
   res.json({ authentificated: false, user: null });
 }
