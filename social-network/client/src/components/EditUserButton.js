fca20ae1a4d41edf6c66beec30ef6558bb406930
kostyniuk <kostyniuk7@gmail.com>
2020-06-22 14:49:18 +0300

    add a new button

---
+++ social-network/client/src/components/EditUserButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditUserButton = () => {
+  return (
+    <div>
+      <button className='btn btn-secondary'>Edit Profile</button>
+    </div>
+  );
+};
+
+export default EditUserButton;
