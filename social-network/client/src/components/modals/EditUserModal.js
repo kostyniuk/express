2a955fdd6aa472627cd4b200367f8028b611c13a
kostyniuk <kostyniuk7@gmail.com>
2020-06-22 14:49:05 +0300

    dress it up

---
+++ social-network/client/src/components/modals/EditUserModal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import EditPostModal from './EditPostModal';
+
+const EditUserModal = () => {
+  return (
+    <div><h2>Edit User</h2></div>
+  )
+}
+
+export default EditPostModal;
